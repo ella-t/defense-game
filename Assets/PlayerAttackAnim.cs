07d8bad32598c7200c2de1925740bc51dcf61787
ella-t <58817053+ella-t@users.noreply.github.com>
2022-05-27 18:19:36 +0100

    Game Play

--- Assets/PlayerAttackAnim.cs
+++ Assets/PlayerAttackAnim.cs
@@ -3,31 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(Collider2D))]
 public class PlayerAttackAnim : MonoBehaviour
 {
+
+    // Inspector
+    [SerializeField]
+    private float _moveSpeed;
 
     // Memory
     private ParticleSystem _particles;
     private PlayerControl _player;
     private float _intensity;
+    private Collider2D _collider;
 
     private void Start()
     {
         _particles = GetComponent<ParticleSystem>();
         _player = FindObjectOfType<PlayerControl>();
         _intensity = 1.0f;
+        _collider = GetComponent<Collider2D>();
     }
 
     private void Update()
     {
         _intensity = Mathf.Lerp(0, 1, _player.AttackTimeRemaining / _player.MaxAttackTime);
 
-        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (_player.Attacking)
-            _particles.Play();
+        {
+            if (_particles.isPlaying == false)
+            {
+                _particles.Play();
+                transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+            }
+
+            //transform.position = Vector3.Lerp(transform.position, new Vector3(mousePos.x, mousePos.y, transform.position.z), _moveSpeed * Time.deltaTime);
+            Vector3 moveDir = (new Vector3(mousePos.x, mousePos.y, transform.position.z) - transform.position).normalized;
+            transform.Translate(moveDir * _moveSpeed * Time.deltaTime);
+
+            CheckEnemyCollision();
+        }
         else
-            _particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        {
+            if (_particles.isPlaying)
+            {
+                _particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            }
+        }
+
+    }
+
+    private void CheckEnemyCollision()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        foreach (Enemy e in enemies)
+        {
+            if (_collider.bounds.Intersects(e.Collider.bounds))
+            {
+                print("Collide");
+                e.Die();
+                continue;
+            }
+        }
     }
 
 
--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -11,19 +11,39 @@ public class PlayerControl : MonoBehaviour
     private float _attackTime;
     [SerializeField]
     private Tower _tower;
+    [SerializeField]
+    private float _towerCooldown;
 
     // Public info
     public bool Attacking { get; private set; }
     public float AttackTimeRemaining { get; private set; }
     public float MaxAttackTime { get { return _attackTime; } private set { _attackTime = value; } }
 
+    // Memory
+    private Collider2D _collider;
+    private float _towerCooldownRemaining;
+
     private void Start()
     {
         AttackTimeRemaining = 0;
+        _collider = GetComponent<Collider2D>();
+        _towerCooldownRemaining = 0;
     }
 
     private void Update()
     {
+        if (_towerCooldownRemaining <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceTower();
+            }
+        }
+        else
+        {
+            _towerCooldownRemaining -= Time.deltaTime;
+        }
+        
         if (Attacking)
         {
             AttackTimeRemaining -= Time.deltaTime;
@@ -38,6 +58,16 @@ public class PlayerControl : MonoBehaviour
     {
         AttackTimeRemaining = _attackTime;
         Attacking = true;
+    }
+
+    private void PlaceTower()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (_collider.bounds.Contains(new Vector3(mousePos.x, mousePos.y, transform.position.z)) == false)
+        {
+            _towerCooldownRemaining = _towerCooldown;
+            Instantiate(_tower, new Vector3(mousePos.x, mousePos.y, transform.position.z), Quaternion.identity);
+        }
     }
 
 }
